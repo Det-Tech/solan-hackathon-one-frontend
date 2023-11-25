920789dc6c957a8f4dd59b64875074544e3855df
Det-tech <proninkirilll228@gmail.com>
2023-11-25 12:52:36 -0800

    Updated all product page

---
+++ Components/Products/Products.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+//INTERNAL IMPORT
+import Style from "./Products.module.css";
+import BlogCard from "../BlogCard/BlogCard";
+
+import { getProducts } from "../../api";
+import { Grid } from "@mui/material";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((data) => {
+      if (data.success) setProducts(data.message);
+    });
+  }, []);
+
+  return (
+    <div className={Style.blog_section}>
+      <h1 className={Style.blog_section_title}>&nbsp;&nbsp;&nbsp;&nbsp;Posted Products</h1>
+      <Grid container>
+        {products.map((product, index) => (
+          <Grid xs={12} sm={6} md={4} lg={3} sx={{padding:"20px"}} item>
+          <BlogCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Products;
